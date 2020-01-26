af3bcbcd8c7cf4bebf1cf2fb93adcf4c3acfef82
Devin <54780446+barnacleDevelopments@users.noreply.github.com>
2020-01-26 10:23:41 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var bodyParser = require("body-parser"),
     expressSanitizer = require("express-sanitizer")
     
 //models 
-const Blog = require("./models/Blog")
+const blogRoute = require("./routes/blog-routes")
+app.use("/", blogRoute)
 
     mongoose.set('useFindAndModify', false);
     var methodOverride = require("method-override")
@@ -23,88 +24,7 @@ const Blog = require("./models/Blog")
     });
 
     //get all the blogs
-    app.get("/blogs", (req, res) => {
-        Blog.find({}, (err, blogs) => {
-            if(err){
-                console.log(err)
-            } else {
-                res.render("index", {blogs: blogs});
-            }
-        });
-    });
 
-    app.get("/blogs/new", (req, res) => {
-        res.render("new")
-    });
-
-    //create blog post
-    app.post("/blogs", (req, res) => {
-        req.body.content = req.sanitize(req.body.content);
-        let blog = {
-            title: req.body.title,
-            content: req.body.content,
-            image: req.body.image
-        }
-
-            Blog.create(blog, (err, blogs) => {
-                if(err){
-                    res.render("new")
-                } else {
-                    console.log(blogs)
-                    res.redirect("/blogs")
-                }
-            });
-    });
-
-    //show a focused view of a single sellected blog post
-    app.get("/blogs/:id", (req, res) => {
-        Blog.findById(req.params.id, (err, singleBlog) => {
-            if(err) {
-                res.redirect("/blogs")
-            } else {
-                res.render("show", {blog: singleBlog});
-            }
-        });
-    });
-
-    //render edit page 
-    app.get("/blogs/:id/edit", (req, res) => {
-        Blog.findById(req.params.id, (err, blogEdit) => {
-            if(err){
-                console.log(err);
-                res.redirect("/blogs"); 
-            } else {
-                res.render("edit", {blog: blogEdit});
-            }
-        });
-    });
-    
-
-    app.put("/blogs/:id", (req, res) => {
-        let blog = {
-            title: req.body.title,
-            content: req.body.content,
-            image: req.body.image
-        }
-
-         Blog.findByIdAndUpdate(req.params.id, blog, (err, updatedBlog) => {
-              if(err) {
-                  console.log(err)
-             } else {
-                 res.redirect("/blogs/" + updatedBlog._id)
-             }
-        });
-    });
-
-    app.delete("/blogs/:id", (req, res) => {
-        Blog.findOneAndDelete(req.params.id, req.body.blog, (err) => {
-            if(err) {
-                console.log(err);
-            } else {
-                res.redirect("/blogs");
-            }
-        })
-    })
 
     app.listen(3000, () => {
         console.log("Blog started!")        
---
+++ routes/blog-routes.js
@@ -0,0 +1,96 @@
+const express  = require("express")
+const app      = express()
+const router    = express.Router()
+const mongoose = require("mongoose")
+const Blog     = require("../models/Blog")
+const expressSanitizer = require("express-sanitizer")
+const bodyParser   = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
+
+router.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if(err){
+            console.log(err)
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+router.get("/blogs/new", (req, res) => {
+    res.render("new")
+});
+
+//create blog post
+router.post("/blogs", (req, res) => {
+    req.body.content = req.sanitize(req.body.content);
+    let blog = {
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image
+    }
+
+        Blog.create(blog, (err, blogs) => {
+            if(err){
+                res.render("new")
+            } else {
+                console.log(blogs)
+                res.redirect("/blogs")
+            }
+        });
+});
+
+//show a focused view of a single sellected blog post
+router.get("/blogs/:id", (req, res) => {
+    Blog.findById(req.params.id, (err, singleBlog) => {
+        if(err) {
+            res.redirect("/blogs")
+        } else {
+            res.render("show", {blog: singleBlog});
+        }
+    });
+});
+
+//render edit page 
+router.get("/blogs/:id/edit", (req, res) => {
+    Blog.findById(req.params.id, (err, blogEdit) => {
+        if(err){
+            console.log(err);
+            res.redirect("/blogs"); 
+        } else {
+            res.render("edit", {blog: blogEdit});
+        }
+    });
+});
+
+
+router.put("/blogs/:id", (req, res) => {
+    let blog = {
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image
+    }
+
+     Blog.findByIdAndUpdate(req.params.id, blog, (err, updatedBlog) => {
+          if(err) {
+              console.log(err)
+         } else {
+             res.redirect("/blogs/" + updatedBlog._id)
+         }
+    });
+});
+
+router.delete("/blogs/:id", (req, res) => {
+    Blog.findOneAndDelete(req.params.id, req.body.blog, (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+})
+
+module.exports = router
