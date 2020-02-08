11dc8975ee1db3609dce1b09e65d2f13d9b6e0a0
Devin <54780446+barnacleDevelopments@users.noreply.github.com>
2020-02-08 16:16:02 -0400

    Add files via upload

--- public/js/blog-searcher.js
+++ public/js/blog-searcher.js
@@ -1,46 +1,77 @@
-const searchInput = document.getElementById("blog-search")
-const searchContainer = document.getElementById(("searched-items"))
+const searchInput     = document.getElementById("search-input")
+const searchContainer = document.getElementById("search-items")
 
-searchInput.addEventListener("input", (e) => {
-    const searchQuery = e.target.value
-    var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        handleData(JSON.parse(this.responseText), searchQuery)
+searchInput.addEventListener("input", () => {
+    const searchQuery = searchInput.value
+    if(!searchQuery) {
+       searchInput.dataset.state = ""
     }
-  };
-  xhttp.open("GET", "/blogs/search", true);
-  xhttp.send();
+
+    const trimmed = searchQuery.trim()
+
+    if(trimmed) {
+        searchInput.dataset.state = "valid"
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              handleData(JSON.parse(this.responseText), searchQuery)
+          }
+        };
+        xhttp.open("GET", "/blogs/search", true);
+        xhttp.send();
+    } else {
+        searchInput.dataset.state = "invalid"
+        emptySearchSuggestions()
+    }
+
+
 })
 
-const handleData = (data, searchQuery) => {
-    const searchedItems = data.map((d) => {
-        return titleLetters = d.title
-    })
-    
-    let searchedContainer = []
 
-    searchedItems.map((item) => {
-        if(item.includes(searchQuery)) {
-            searchedContainer.push(item)
-        } 
-    })
-    
-    sendResults(searchedContainer)
+const handleData = (data, searchQuery) => {    
+   const blogData = data.map((data) => {
+       return {
+           title: data.title.toLowerCase(),
+           id: data._id
+       }
+   })
+
+    blogData.forEach((blog) => {
+        if(blog.title.includes(searchQuery)) {
+                createSearchSuggestion(blog)
+        }
+    }) 
 }
 
+const createSearchSuggestion = (blog) => {
 
-const sendResults = searchedContainer => {
-    let list     = document.createElement("ul")
-    searchedContainer.forEach((item) => {
-        let itemText = document.createTextNode(item)
+        let blogTitleText = document.createTextNode(blog.title)
+        let link          = document.createElement("a")
+        link.appendChild(blogTitleText)
+        link.href = `/blogs/${blog.id}`
         let listItem = document.createElement("li")
-        listItem.appendChild(itemText)
-        list.appendChild(listItem)
-    })
-
-    searchContainer.appendChild(list)
-
-
-   
+        listItem.appendChild(link)
+        searchContainer.appendChild(listItem) 
 }
+
+const emptySearchSuggestions= () => {
+    while(searchContainer.firstChild) {
+        searchContainer.removeChild(searchContainer.firstElementChild)
+    }
+
+}
+
+window.addEventListener("click", (e) => {
+    e.target.value = ""
+    emptySearchSuggestions() 
+})
+
+searchInput.addEventListener("focus", (e) => {
+    emptySearchSuggestions()
+    e.target.value = ""
+})
+
+
+
+  
+
